68f78e7b7c7faa82c668bb8f7aa67edb11f0a798
matheus souza <ruivorobado@gmail.com>
2021-06-18 12:06:22 -0300

    salvamento no banco de dados feito

--- ediaristas/web/views.py
+++ ediaristas/web/views.py
@@ -5,5 +5,9 @@ from .forms import diarista_form
 # Create your views here.
 
 def cadastrar_diarista(request):
+    if request.method == 'POST':
+        form_diarista = diarista_form.DiaristaForm(request.POST)
+        if form_diarista.is_valid():
+            form_diarista.save()
     form_diarista = diarista_form.DiaristaForm()
     return render(request, 'form_diarista.html', {'form_diarista': form_diarista})
